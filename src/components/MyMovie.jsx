c0bfc0c053d3af3d11f4d26c3b4207312ae53284
Umar Ali <umarcodev@gmail.com>
2024-07-31 16:41:07 +0500

    made all screen responsive

--- src/components/MyMovie.jsx
+++ src/components/MyMovie.jsx
@@ -38,22 +38,23 @@ const MyMovie = () => {
 
   return (
     <div className="bg-[#093545] w-full h-auto flex flex-col gap-24 justify-between">
-      <div className="w-[85%] mx-auto flex flex-col  mt-[120px]">
+      <div className="w-[85%] mx-auto flex flex-col sm:mt-[120px] mt-[80px]">
+
         {/* Upper section  */}
         <div className="flex justify-between">
-          <Link to="/add-movie" className="flex items-center gap-4">
-            <div className="text-[48px] font-semibold text-white text-start ">
+          <Link to="/add-movie" className="flex items-center sm:gap-4 gap-3">
+            <div className="text-[32px] sm:text-[48px] font-semibold text-white text-start ">
               My Movies
             </div>
-            <img src={addIcon} alt="" className="w-[32px] h-[32px] mt-[8px]" />
+            <img src={addIcon} alt="" className="sm:w-[32px] w-[20px] h-[20px] sm:h-[32px] mt-[8px]" />
           </Link>
 
           <div
             className="flex items-center gap-3 cursor-pointer"
             onClick={handleLogout}
           >
-            <div className="text-[16px] font-bold text-white">Logout</div>
-            <img src={logoutIcon} alt="" className="w-[32px] h-[32px]" />
+            <div className="text-[16px] font-bold text-white sm:block hidden">Logout</div>
+            <img src={logoutIcon} alt="" className="w-[32px] h-[32px] sm:w-[32px] sm:h-[32px]" />
           </div>
         </div>
 
@@ -61,16 +62,16 @@ const MyMovie = () => {
         <div className="flex flex-wrap gap-3 mt-[120px] ">
           {currentPosts.map((ele, index) => (
             <div
-              className="w-[282px]  h-[510px] bg-[#092C39] rounded-xl mb-6"
+              className='w-[180px] sm:w-[282px] sm:mt-[120px] mt-[80px] sm:h-[510px] h-[334px] bg-[#092C39] rounded-xl'
               key={index}
             >
               <img
                 src={ele.image}
                 alt=""
-                className="w-[266px] h-[400px] border-2 mx-auto mt-2 rounded-xl"
+                className='w-[180px] h-[246px] sm:w-[266px] sm:h-[400px] border-2 mx-auto mt-2 rounded-xl'
               />
               <div className="pl-3 flex flex-col gap-3 my-4 text-white">
-                <div className="text-[20px]">{ele.category}</div>
+                <div className="sm:text-[20px] text-[16px]">{ele.category}</div>
                 <div className="font-normal text-sm">{ele.price}</div>
               </div>
             </div>
